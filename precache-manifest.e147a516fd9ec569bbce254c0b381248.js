5bfb3b58f03a3d8cac008406747bcef2065d298c
Bunny <vi.chybirka@gmail.com>
2019-06-13 00:55:25 +0300

    start project

---
+++ precache-manifest.e147a516fd9ec569bbce254c0b381248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-license/robots.txt"
+  },
+  {
+    "revision": "235b12d0efb89f5aebaa",
+    "url": "/vue-license/js/chunk-vendors.b7f6ef16.js"
+  },
+  {
+    "revision": "4aa17b6b2b2f360ae845",
+    "url": "/vue-license/js/app.17fff62c.js"
+  },
+  {
+    "revision": "e01b178d379b0955039a53ac9ff6756e",
+    "url": "/vue-license/index.html"
+  },
+  {
+    "revision": "235b12d0efb89f5aebaa",
+    "url": "/vue-license/css/chunk-vendors.2039f433.css"
+  },
+  {
+    "revision": "4aa17b6b2b2f360ae845",
+    "url": "/vue-license/css/app.c14c1a84.css"
+  }
+];
